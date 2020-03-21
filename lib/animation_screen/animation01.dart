d3a22b6fbb1aa19c8cdd4b9af26b0e2c948c00c6
Atri Das <babinsamrat123@gmail.com>
2020-03-21 13:30:39 +0530

    #2 infinity animation

--- lib/animation_screen/animation01.dart
+++ lib/animation_screen/animation01.dart
@@ -9,33 +9,38 @@ class AnimationFirst extends StatefulWidget {
 class _AnimationFirstState extends State<AnimationFirst>
     with SingleTickerProviderStateMixin {
 
-  AnimationController _animationController;
-  Animation<double> _animation;
+  AnimationController animationController;
+  Animation<double> animation;
 
   @override
   void initState() {
     super.initState();
-    _animationController = AnimationController(
+    animationController = AnimationController(
       duration: Duration(seconds: 5),
       vsync: this,
     );
 
-    _animation = Tween<double>(begin: 0.0,end: 2 * math.pi)
-        .animate(_animationController)
+    final curvedAnimation = CurvedAnimation(
+      parent: animationController,
+      curve: Curves.bounceIn,
+      reverseCurve: Curves.easeOut,
+    );
+
+    animation = Tween<double>(
+      begin: 0.0,
+      end: 2 * math.pi,
+    ).animate(animationController/*curvedAnimation*/)
     ..addListener((){
-      setState(() {
-
-      });
+      setState(() {});
+    })
+    ..addStatusListener((status){
+      if(status == AnimationStatus.completed){
+        animationController.reverse();
+      }else if(status == AnimationStatus.dismissed){
+        animationController.forward();
+      }
     });
-
-
-    _animationController.forward();
-  }
-
-  @override
-  void dispose() {
-    _animationController.dispose();
-    super.dispose();
+    animationController.forward();
   }
 
   @override
@@ -46,7 +51,7 @@ class _AnimationFirstState extends State<AnimationFirst>
         backgroundColor: Colors.green[900],
       ),
       body: Transform.rotate(
-        angle: _animation.value,
+        angle: animation.value,
         child: Container(
           alignment: Alignment.center,
           padding: EdgeInsets.all(30.0),
@@ -54,5 +59,11 @@ class _AnimationFirstState extends State<AnimationFirst>
         ),
       ),
     );
+  }
+
+  @override
+  void dispose() {
+    animationController.dispose();
+    super.dispose();
   }
 }
