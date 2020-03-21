061a9840e319ede50a82d031c49e4ce3fc328ce4
Atri Das <babinsamrat123@gmail.com>
2020-03-21 13:06:03 +0530

    #1 initial Animation

--- lib/animation_screen/animation01.dart
+++ lib/animation_screen/animation01.dart
@@ -1,16 +1,42 @@
-import 'dart:ffi';
-
 import 'package:flutter/material.dart';
-
+import 'dart:math' as math;
 
 class AnimationFirst extends StatefulWidget {
   @override
   _AnimationFirstState createState() => _AnimationFirstState();
 }
 
-class _AnimationFirstState extends State<AnimationFirst> with SingleTickerProviderStateMixin{
+class _AnimationFirstState extends State<AnimationFirst>
+    with SingleTickerProviderStateMixin {
 
   AnimationController _animationController;
+  Animation<double> _animation;
+
+  @override
+  void initState() {
+    super.initState();
+    _animationController = AnimationController(
+      duration: Duration(seconds: 5),
+      vsync: this,
+    );
+
+    _animation = Tween<double>(begin: 0.0,end: 2 * math.pi)
+        .animate(_animationController)
+    ..addListener((){
+      setState(() {
+
+      });
+    });
+
+
+    _animationController.forward();
+  }
+
+  @override
+  void dispose() {
+    _animationController.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -20,7 +46,7 @@ class _AnimationFirstState extends State<AnimationFirst> with SingleTickerProvid
         backgroundColor: Colors.green[900],
       ),
       body: Transform.rotate(
-        angle: 0.7,
+        angle: _animation.value,
         child: Container(
           alignment: Alignment.center,
           padding: EdgeInsets.all(30.0),
